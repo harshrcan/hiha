c58e2b38057e79d96757157e11fd773961c532e6
harshrcan <harsh.rana1@student.kpu.ca>
2025-07-06 16:07:16 -0700

    Add files via upload

---
+++ alerts.js
@@ -0,0 +1,29 @@
+// alerts.js
+
+function showMenuSavedAlert() {
+  alert('Menu selection saved! You can now proceed to booking.');
+}
+
+function showChefRequiredAlert() {
+  alert('Please select a chef first!');
+}
+
+function showBookingRedirectAlert() {
+  alert('Please select your menu items before booking.');
+}
+
+function showBookingSuccessAlert() {
+  alert('Reservation submitted successfully! We will contact you within 24 hours to confirm your booking.');
+}
+
+function showBookingErrorAlert() {
+  alert('There was an error submitting your reservation. Please try again or contact us directly.');
+}
+
+function showContactSuccessAlert() {
+  alert('Thank you for your message! We will get back to you within 24 hours.');
+}
+
+function showContactErrorAlert() {
+  alert('There was an error sending your message. Please try again or contact us directly.');
+} 
